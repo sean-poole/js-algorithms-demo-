e21cb98bc640da83dc72fce6517c3863c1d8a400
Sean Poole <spoole5289@gmail.com>
2024-05-01 12:35:12 -0700

    complete reverseString functions

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,9 +4,49 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
+// Built-in methods: 
+/* Slowest */
+// function reverseString(text) {
+//   return text.split("").reverse().join("");
+// }
 
+/* Fastest */
+// function reverseString(text) {
+//   return text.split("").reduce((acc, c) => c + acc, "");
+// }
+
+
+// For Loop: 
+// function reverseString(text) {
+//   let result = "";
+
+//   for (let i = text.length - 1; i >= 0; i--) {
+//     result += text[i];
+//   }
+
+//   return result;
+// }
+
+
+// For Of: 
+// function reverseString(text) {
+//   let result = "";
+
+//   for (let c of text) {
+//     result = c + result;
+//   }
+
+//   return result;
+// }
+
+
+// Recursion: 
 function reverseString(text) {
-    // Code goes here
+  if (text === "") {
+    return "";
+  } else {
+    return reverseString(text.substr(1)) + text[0];
+  }
 }
 
 
