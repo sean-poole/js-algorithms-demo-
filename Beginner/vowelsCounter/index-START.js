b7303a332e382b5a4522cf08bfb20c173ce201d9
Sean Poole <spoole5289@gmail.com>
2024-05-01 13:15:27 -0700

    complete vowelsCounter function

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -4,8 +4,30 @@ e.g vowelsCounter('anehizxcv') // will return 3
 */
 
 
+// ITERATIVE (slowest): 
+// function vowelsCounter(text) {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   let count = 0;
+
+//   for (c of text.toLowerCase()) {
+//     if (vowels.includes(c)) count++;
+//   }
+
+//   return count;
+// }
+
+
+// REGULAR EXPRESSIONS (fastest): 
 function vowelsCounter(text) {
-    // Code goes here
+  /*
+    Regular expression defined within the slashes.
+    Letters within the brackets specify what to look for.
+    g - global search: will not start over from the beginning after finding the first match.
+    i - case insensitive search: makes the entire express case-insensitive.
+  */
+  let matchingVowels = text.match(/[aeiou]/gi);
+
+  return matchingVowels ? matchingVowels.length : 0;
 }
 
 
