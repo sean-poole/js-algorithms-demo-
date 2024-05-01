39b6706184c9550c0d6564ff431952ea4972baa0
Sean Poole <spoole5289@gmail.com>
2024-05-01 16:24:53 -0700

    complete maxRecurringCharacter function

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -4,10 +4,52 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 */
 
 
-
+// FOR-IN ITERATION (fastest): 
 function maxRecurringChar(text) {
-    // Code goes here
+  let charMap = {};
+  let maxValue = 0;
+  let maxChar = "";
+
+  for (let c of text) {
+    if (charMap[c]) {
+      charMap[c] += 1;
+    } else {
+      charMap[c] = 1;
+    }
+  }
+
+  for (let c in charMap) {
+    if (charMap[c] > maxValue) {
+      maxValue = charMap[c];
+      maxChar = c;
+    }
+  }
+
+  return maxChar;
 }
+
+
+// FORM ARRAY FROM CHARACTER MAP (slowest): 
+// function maxRecurringChar(text) {
+//   let charMap = {};
+//   let charArray = [];
+//   let valuesArray = [];
+//   let maxValue = 0;
+
+//   for (let c of text) {
+//     if (charMap[c]) {
+//       charMap[c] += 1;
+//     } else {
+//       charMap[c] = 1;
+//     }
+//   }
+
+//   charArray = Object.keys(charMap);
+//   valuesArray = Object.values(charMap);
+//   maxValue = Math.max(...valuesArray);
+
+//   return charArray[valuesArray.indexOf(maxValue)];
+// }
 
 
 
