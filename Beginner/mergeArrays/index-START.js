36e23d394532556562390e618899cefa57e08eeb
Sean Poole <spoole5289@gmail.com>
2024-05-02 16:47:07 -0700

    complete mergeArrays function

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,42 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+
+// SETS (slowest): 
+// function mergeArrays(...arrays) {
+//   let jointArray = [];
+
+//   arrays.forEach(array => {
+//     jointArray = [...jointArray, ...array];
+//   });
+
+//   return [...new Set([...jointArray])];
+// }
+
+
+// FILTER METHOD (fastest): 
 function mergeArrays(...arrays) {
+  let jointArray = [];
 
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
+  arrays.forEach(array => {
+    jointArray = [...jointArray, ...array];
+  });
 
-    return [...new Set([...jointArray])]
-
-    
+  return jointArray.filter((e, i) => jointArray.indexOf(e) === i);
 }
+
+
+// REDUCE METHOD: 
+// function mergeArrays(...arrays) {
+//   let jointArray = [];
+
+//   arrays.forEach(array => {
+//     jointArray = [...jointArray, ...array];
+//   });
+
+//   return jointArray.reduce((acc, c) => acc.includes(c) ? acc : [...acc, c], []);
+// }
+
 
 
 module.exports = mergeArrays
