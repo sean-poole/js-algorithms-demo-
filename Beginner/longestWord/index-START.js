0b972d080c1d536780549ec6b31ed7a7f2b455a6
Sean Poole <spoole5289@gmail.com>
2024-05-02 14:25:09 -0700

    complete longestWord function

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -4,10 +4,44 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
+// FOR-LOOP (fastest):
+// function longestWord(text) {
+//   let wordsArr = text.split(" ");
+//   let longestWord = "";
+
+//   for (let word of wordsArr) {
+//     if (word.length >= longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
+
+//   return longestWord;
+// }
+
+
+// REDUCE METHOD (fastest): 
+// function longestWord(text) {
+//   let result = text.split(" ").reduce((maxLenWord, currentWord) => {
+//     if (currentWord.length > maxLenWord.length) {
+//       return currentWord;
+//     } else {
+//       return maxLenWord;
+//     }
+//   }, "");
+
+//   return result;
+// }
 
 function longestWord(text) {
-    // Code goes here
+  return text.split(" ").reduce((maxWord, currentWord) => currentWord.length > maxWord.length ? currentWord : maxWord);
 }
+
+
+// SORT METHOD (slowest): 
+// function longestWord(text) {
+//   return text.split(" ").sort((a, b) => b.length - a.length)[0];
+// }
+
 
 
 module.exports = longestWord
