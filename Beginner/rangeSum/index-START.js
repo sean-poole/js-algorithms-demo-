28c4f2c080f588c3524cb3c38e052f13e2350c61
Sean Poole <spoole5289@gmail.com>
2024-05-03 14:42:36 -0700

    complete rangeSum function

--- Beginner/rangeSum/index-START.js
+++ Beginner/rangeSum/index-START.js
@@ -1,5 +1,34 @@
+/* CHALLENGE: 
+Given a two element array of integers, calculate the sum of all numbers from the first element to the second element.
+E.g.
+  rangeSum([1, 4])  // returns 10
+*/
+
+
+// FOR LOOP: 
+// function rangeSum(arr) {
+//   let min = Math.min(...arr);
+//   let max = Math.max(...arr);
+//   let sum = 0;
+
+//   for (let i = min; i <= max; i++) {
+//     sum += i;
+//   }
+
+//   return sum;
+// }
+
+
+// SUMMATION (fastest): 
 function rangeSum(arr) {
-  // Code goes here
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  
+  let sum = ((max - min + 1) * (min + max)) / 2;
+
+  return sum;
 }
+
+console.log(rangeSum([1, 4]))   // 10
 
 module.exports = rangeSum;
