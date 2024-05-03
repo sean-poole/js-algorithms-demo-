3ee08cb6afeacf378b61925b1aeca5ceaa88388a
Sean Poole <spoole5289@gmail.com>
2024-05-03 13:17:28 -0700

    complete falsyBouncer function

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -6,9 +6,25 @@ E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
 
+// FOR-OF LOOP (fastest): 
 function falsyBouncer(array) {
-    // Code goes here
+  let result = [];
+
+  for (let el of array) {
+    if (el) {
+      result.push(el);
+    }
+  }
+
+  return result;
 }
+
+
+// FILTER METHOD (slowest): 
+// function falsyBouncer(array) {
+//   return array.filter(e => e);
+// }
+
 
 
 module.exports = falsyBouncer
