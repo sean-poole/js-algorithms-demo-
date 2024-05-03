4de92f5c2ded300455fc2be8026902a432152d1c
Sean Poole <spoole5289@gmail.com>
2024-05-03 14:28:47 -0700

    complete integerReversal function

--- Beginner/IntegerReversal/index-START.js
+++ Beginner/IntegerReversal/index-START.js
@@ -1,17 +1,20 @@
 /* CHALLENGE
 Given an integer, return an integer that is the reverse 
 ordering of that which was received. E.g
-    reverseInteger(-123) // should return -321
+  reverseInteger(-123) // should return -321
 */
 
 
+// REVERSE A STRING: 
 function reverseString(text) {
-    return [...text].reduce((acc, char) => char + acc, '')
+  return [...text].reduce((acc, char) => char + acc, '')
 }
 
+
+// REVERSE AN INTEGER: 
 function reverseInteger(num) {
-    let reversedNumber = parseInt(reverseString(num.toString()))
-    return (reversedNumber * Math.sign(num))
+  let reversedNumber = parseInt(reverseString(num.toString()))
+  return (reversedNumber * Math.sign(num))
 }
 
 
